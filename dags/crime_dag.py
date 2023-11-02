7081fee054ef5a27b4d1e123882f28529f98c90f
ferrysetefanus <psandrezzz19@gmail.com>
2023-11-02 16:44:39 +0700

    update spark scripts

--- dags/crime_dag.py
+++ dags/crime_dag.py
@@ -3,7 +3,9 @@ from airflow.utils.dates import days_ago
 from datetime import timedelta
 from airflow.operators.bash import BashOperator
 from airflow.operators.dummy import DummyOperator
-import gdown
+from airflow.operators.python import PythonOperator
+from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
+import requests
 
 default_args = {
     "owner": "ferry",
@@ -12,6 +14,34 @@ default_args = {
 
 def extract_file():
     url = "https://data.lacity.org/api/views/2nrs-mtv8/rows.csv?accessType=DOWNLOAD"
-    output = 'opt/airflow/datasets/la_crime.csv'
-    gdown.download(url, output, quiet=False)
+    output = '/opt/airflow/datasets/la_crime.csv'
+    
+    with requests.get(url, stream=True) as response:
+        with open(output, 'wb') as f:
+            for chunk in response.iter_content(chunk_size=1024):
+                if chunk:
+                    f.write(chunk)
 
+
+with DAG(
+    dag_id="crime_dag",
+    default_args=default_args,
+    dagrun_timeout=timedelta(minutes=60),
+    description="dag for crime analysis",
+    start_date=days_ago(1)
+) as dag:
+    
+    start = DummyOperator(task_id="start")
+
+    extract_file = PythonOperator(
+        task_id="extract_file",
+        python_callable=extract_file
+    )
+
+    transform = SparkSubmitOperator(
+        
+    )
+
+    end = DummyOperator(task_id="end")
+
+    start >> extract_file >> end
--- spark-scripts/spark_transform.py
+++ spark-scripts/spark_transform.py
@@ -5,6 +5,40 @@ from pathlib import Path
 import pyspark.sql.functions as F
 from pyspark.sql.types import *
 
+dotenv_path = Path('../.env')
+load_dotenv(dotenv_path=dotenv_path)
+
+postgres_host = os.getenv('POSTGRES_HOST')
+metabase_db = os.getenv('METABASE_DB')
+postgres_user = os.getenv('POSTGRES_USER')
+postgres_password = os.getenv('POSTGRES_PASSWORD')
+
+spark_master = os.getenv("SPARK_MASTER_HOST_NAME")
+spark_port = os.getenv("SPARK_MASTER_PORT")
+script_path = "/spark-scripts/postgresql-42.6.0.jar"
+
+sparkcontext = pyspark.SparkContext.getOrCreate(conf=(
+        pyspark
+        .SparkConf()
+        .setAppName('la_crime')
+        .setMaster(f'spark://{spark_master}:{spark_port}')
+        .set("spark.jars", script_path)
+    ))
+
+spark = pyspark.sql.SparkSession(sparkcontext.getOrCreate())
+
+jdbc_url = f'jdbc:postgresql://{postgres_host}/{metabase_db}'
+jdbc_properties = {
+    'user': postgres_user,
+    'password': postgres_password,
+    'driver': 'org.postgresql.Driver',
+    'stringType': 'unspecified'
+}
+
+crime_df = spark.read.csv("/opt/datasets/la_crime.csv")
+
+transformed_crime_df = crime_df.select("*")
+
 
 
 
